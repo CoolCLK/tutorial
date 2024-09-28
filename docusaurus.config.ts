3cf0cbd27e22eb9a720a328ad10a4d84c0c7ad9a
CoolCLK <mr.chenlk123@qq.com>
2024-09-28 14:02:09 +0800

    Commit

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -40,13 +40,6 @@ const config: Config = {
           editUrl:
             'https://github.com/CoolCLK/tutorial/tree/main/',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/CoolCLK/tutorial/tree/main/',
-        },
         theme: {
           customCss: './src/css/custom.css',
         },
@@ -70,7 +63,6 @@ const config: Config = {
           position: 'left',
           label: 'Tutorial',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
         {
           href: 'https://github.com/facebook/docusaurus',
           label: 'GitHub',
@@ -110,10 +102,6 @@ const config: Config = {
         {
           title: 'More',
           items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
             {
               label: 'GitHub',
               href: 'https://github.com/facebook/docusaurus',
